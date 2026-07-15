f4ac651dec8c7eb2133f86dc034013d8610bfd57
avinashkale14 <avinashkale8263@gmail.com>
2026-07-15 16:26:52 +0530

    Prepared project for deployment

--- app.js
+++ app.js
@@ -75,6 +75,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error.ejs", { message });
 });
 
-app.listen(8080, () => {
-    console.log("Server running on port 8080 🚀");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} 🚀`);
 });
